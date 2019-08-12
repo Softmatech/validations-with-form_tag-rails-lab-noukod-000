d7cbb58fbc9c292af2ddfae2c88bee6a03565ae6
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-12 15:41:30 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,23 +1,23 @@
 class PostsController < ApplicationController
-  def show
-    @post = Post.find(params[:id])
-  end
+    def show
+      @post = Post.find(params[:id])
+    end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+    def edit
+      @post = Post.find(params[:id])
+    end
 
-  def update
-    @post = Post.find(params[:id])
+    def update
+      @post = Post.find(params[:id])
 
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
-  end
-
-  private
-
-  def post_params
-    params.permit(:title, :category, :content)
-  end
+      if @post.update(post_params)
+        redirect_to post_path(@post)
+      else
+        render :edit
+      end
+    end
+    private
+    def post_params
+      params.permit(:title, :category, :content)
+    end
 end
