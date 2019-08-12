9237f9f93aa8e6ceab8ff398bf1f9305c0ead2d1
Andy Feidje Joseph <ingandy75@gmail.com>
2019-08-12 16:57:47 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 
   validates :title, presence: true
   validates :content, length: { minimum: 100 }
-  validates :category, inclusion: { in:%(Fiction Non-Fiction) }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
 
 end
